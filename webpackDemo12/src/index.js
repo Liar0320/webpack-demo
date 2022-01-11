f6dc8ffc76abcf792d6a974ce0c4dab7d3fd2c04
lich0320 <100108350@qq.com>
2022-01-11 13:49:31 +0800

    添加树摇的概念

--- webpackDemo12/src/index.js
+++ webpackDemo12/src/index.js
@@ -1,11 +1,12 @@
-import {cude} from './math.js'
-function component () {
-  var element = document.createElement('pre');
-  element.innerHTML = [
-       'Hello webpack!',
-        '5 cubed is equal to ' + cube(5)
-      ].join('\n\n');
+import { cube } from "./math.js";
+import "./index.css";
+import { awaitWrap } from "@cnpm-lich/utils";
+console.log(awaitWrap);
+function component() {
+  var element = document.createElement("pre");
+  element.innerHTML = ["Hello webpack!", "5 cubed is equal to " + cube(5)].join(
+    "\n\n"
+  );
   return element;
 }
-
 document.body.appendChild(component());
--- webpackDemo12/webpack.config.js
+++ webpackDemo12/webpack.config.js
@@ -1,15 +1,19 @@
 /*eslint-disable */
-var path = require('path');
+var path = require("path");
 var modules = {
-    entry:{
-        app:'./src/index.js'
-    },
-    devtool: 'source-map',
-    output:{
-        filename:'[name].bundle.js',
-        path:path.resolve(__dirname,'dist'),
-    },
-    mode:'development'
-}
+  entry:"./src/index.js",
+  devtool: "source-map",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [{ test: /\.css$/, use: "css-loader" }],
+  },
+  mode: "production",
+  optimization: {
+    usedExports: true,
+  },
+};
 
 module.exports = modules;
